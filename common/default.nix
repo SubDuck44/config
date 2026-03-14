81e01679fbe3a4cc6de3af267ff2c8e7ec76b89b
melinda <melinda.stobbe@mail.de>
2026-03-14 18:06:25 +0100

    pipewire: set high quantum & clock rate

--- common/default.nix
+++ common/default.nix
@@ -266,7 +266,54 @@ let inherit (lib) mkMerge getExe; in
 
     pipewire = {
       enable = true;
+      alsa.enable = true;
       pulse.enable = true;
+
+      extraConfig =
+        let
+          rt = {
+            "module.rt.args" = {
+              "rtportal.enabled" = false;
+              "nice.level" = -11;
+            };
+          };
+
+          quantum = 8192;
+          clock = 192000;
+        in
+        {
+          pipewire = {
+            "00-realtime" = rt // {
+              "context.properties" = {
+                "default.clock.max-quantum" = quantum;
+                "default.clock.min-quantum" = quantum;
+                "default.clock.quantum" = quantum;
+                "default.clock.rate" = clock;
+              };
+            };
+          };
+
+          pipewire-pulse = {
+            "00-realtime" = rt;
+          };
+        };
+
+      wireplumber.extraConfig = {
+        "00-realtime" = {
+          "monitor.alsa.rules" = [{
+            matches = [{
+              "device.name" = "~alsa_card.*";
+            }];
+
+            actions = {
+              update-props = {
+                "api.alsa.headroom" = 8192;
+                "api.alsa.period-size" = 256;
+              };
+            };
+          }];
+        };
+      };
     };
 
     keyd = {
