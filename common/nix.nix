4e2f6bb0962c19442aebceac6caf9dc5d254270c
Melinda <melinda.stobbe@mail.de>
2026-05-17 13:21:59 +0200

    add nixremote builder

--- common/nix.nix
+++ common/nix.nix
@@ -32,6 +32,8 @@ in
     nix = {
       package = pkgs.lixPackageSets.latest.lix;
 
+      distributedBuilds = true;
+
       registry.obscura.to = {
         type = "github";
         owner = "42LoCo42";
@@ -42,9 +44,45 @@ in
       settings = {
         allowed-users = [ "@wheel" ];
 
+        builders-use-substitutes = true;
+
         auto-allocate-uids = true;
         use-cgroups = true;
         experimental-features = [ "auto-allocate-uids" "cgroups" ];
+      };
+
+      buildMachines = [{
+        hostName = "nixremote-bunny";
+        protocol = "ssh-ng";
+
+        maxJobs = 4;
+        speedFactor = 4;
+
+        system = "aarch64-linux";
+        supportedFeatures = [
+          "benchmark"
+          "big-parallel"
+          "gccarch-armv8-a"
+          "kvm"
+          "nixos-test"
+        ];
+      }];
+    };
+
+    programs.ssh = {
+      extraConfig = ''
+        Host nixremote-bunny
+        HostName exit.bunny.vpn
+        Port 18213
+        User nixremote
+        Compression yes
+        IdentityFile ${config.aquaris.secret "svc/nixremote"}
+        ConnectTimeout 1
+      '';
+
+      knownHosts = {
+        "[exit.bunny.vpn]:18213".publicKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBbsL7HyOCM56ejtlWqEBG1YzQwX2KmZ3S5KzoGnWh/j";
       };
     };
   };
