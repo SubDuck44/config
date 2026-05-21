9f77a2516c5d79fa60f0ddcf5c19b58724ff9cec
Melinda <melinda.stobbe@mail.de>
2026-05-21 13:44:39 +0200

    phobos: fix bluetooth

--- machines/phobos/default.nix
+++ machines/phobos/default.nix
@@ -24,6 +24,14 @@
     ];
   };
 
+  boot.kernelPatches = [{
+    name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+    patch = pkgs.fetchurl {
+      url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+      hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
+    };
+  }];
+
   home-manager.sharedModules = [{
     wayland.windowManager.hyprland.extraConfig = ''
       monitor = eDP-1, preferred, auto, 1.0
