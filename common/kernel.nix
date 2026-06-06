27dd97809d7630a3a4382c6663869435462da9e4
Melinda <melinda.stobbe@mail.de>
2026-06-06 11:41:25 +0200

    lavd -> pandemonium & enable ananicy

--- common/kernel.nix
+++ common/kernel.nix
@@ -3,11 +3,21 @@
     kernelPackages = pkgs.linuxPackages_zen;
   };
 
+  aquaris.persist.dirs = {
+    "/root/.cache/pandemonium" = { };
+  };
+
   services = {
+    ananicy = {
+      enable = true;
+      package = pkgs.ananicy-cpp;
+      rulesProvider = pkgs.ananicy-rules-cachyos;
+    };
+
     scx = {
       enable = true;
       package = pkgs.scx.rustscheds;
-      scheduler = "scx_lavd";
+      scheduler = "scx_pandemonium";
     };
   };
 }
