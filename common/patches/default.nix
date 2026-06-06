f0cc86a4efa7118ed8ade3cd00839765961dfa7b
Melinda <melinda.stobbe@mail.de>
2026-06-06 11:38:33 +0200

    patches: nom to unstable

--- common/patches/default.nix
+++ common/patches/default.nix
@@ -8,6 +8,17 @@
 
       inherit (obscura.nvidia.entries) nvtop;
 
+      nix-output-monitor = prev.nix-output-monitor.overrideAttrs {
+        version = "2.1.8-unstable-2026-05-22";
+
+        src = prev.fetchFromGitHub {
+          owner = "maralorn";
+          repo = "nix-output-monitor";
+          rev = "0e855e51c1700e35456faa3dee2e50024f602f42";
+          hash = "sha256-8viiPvLkj0vFdG1kgcNuKXoenyTBvKd+GQ62jwbONns=";
+        };
+      };
+
       factorio-space-age = prev.factorio-space-age.override {
         makeDesktopItem = { exec, ... }@args: prev.makeDesktopItem (args // {
           exec = "gamemoderun ${exec}";
