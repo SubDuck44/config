46095619955db05ba810c6bc898e4233ecd8c6b7
Melinda <melinda.stobbe@mail.de>
2026-07-08 13:25:44 +0200

    update special monitor config on phobos to lua

--- machines/phobos/default.nix
+++ machines/phobos/default.nix
@@ -23,17 +23,14 @@
     ];
   };
 
-  # boot.kernelPatches = [{
-  #   name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
-  #   patch = pkgs.fetchurl {
-  #     url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
-  #     hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
-  #   };
-  # }];
-
-  # home-manager.sharedModules = [{
-  #   wayland.windowManager.hyprland.extraConfig = ''
-  #     monitor = eDP-1, preferred, auto, 1.0
-  #   '';
-  # }];
+  home-manager.sharedModules = [{
+    wayland.windowManager.hyprland.extraConfig = ''
+      hl.monitor({
+        output = "eDP-1",
+        mode = "preferred",
+        position = "auto",
+        scale = 1.0,
+      })
+    '';
+  }];
 }
