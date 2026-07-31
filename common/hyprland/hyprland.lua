b34b439a2159a2f0337cb12e3a5481d616dbd1ac
Melinda <melinda.stobbe@mail.de>
2026-07-31 21:35:05 +0200

    remove unnecessary default logic

--- common/hyprland/hyprland.lua
+++ common/hyprland/hyprland.lua
@@ -284,7 +284,7 @@ hl.bind(
 
 -- Example window rules that are useful
 
-local suppressMaximizeRule = hl.window_rule({
+hl.window_rule({
 	-- Ignore maximize requests from all apps. You'll probably like this.
 	name = "suppress-maximize-events",
 	match = { class = ".*" },
@@ -336,23 +336,6 @@ hl.window_rule({
 	border_size = 0,
 	rounding = 0,
 	move = { 0, 0 },
-})
-
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
-hl.window_rule({
-	name = "move-hyprland-run",
-	match = { class = "hyprland-run" },
-
-	move = "20 monitor_h-120",
-	float = true,
 })
 
 hl.window_rule({
